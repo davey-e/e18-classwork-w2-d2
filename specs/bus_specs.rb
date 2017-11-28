967e56de5fe9f02bb5da6aa4623ac28a4e7592b0
Joe Plevin <joe.plevin@hotmail.co.uk>
2017-11-28 13:26:28 +0000

    Added code for bus drive method

--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -13,4 +13,10 @@ def test_has_route_number
   assert_equal(expected, actual)
 end
 
+def test_bus_can_drive
+  expected = "Brum brum"
+  actual = @bus.drive()
+  assert_equal(expected, actual)
+end
+
 end
