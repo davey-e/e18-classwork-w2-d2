263d05637df41ccd349f8bb848123e632cd4914d
Joe Plevin <joe.plevin@hotmail.co.uk>
2017-11-28 13:37:29 +0000

    Added code for passengers attribute & tested

--- bus.rb
+++ bus.rb
@@ -2,13 +2,17 @@ class Bus
 
   attr_reader :route_number
 
-  def initialize(route_number, destination)
+  def initialize(route_number, destination, passengers)
     @route_number = route_number
     @destination = destination
+    @passengers = passengers
 
   end
 
   def drive()
     return "Brum brum"
+  end
+  def has_passengers?()
+    return @passengers
   end
 end
--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -4,7 +4,7 @@ require_relative("../bus")
 
 class TestBus < MiniTest::Test
 def setup()
-  @bus = Bus.new(22, "Edinburgh")
+  @bus = Bus.new(22, "Edinburgh", [])
 end
 
 def test_has_route_number
@@ -19,4 +19,8 @@ def test_bus_can_drive
   assert_equal(expected, actual)
 end
 
+def test_bus_has_passengers
+  expected = 10
+  actual = @bus.has_passengers?
+end
 end
